8efbc0aa51b3635f8ba3ab627e0a11270a19ab3f
Swornim Barahi <sbara002@plattsburgh.edu>
2019-04-09 15:16:04 -0400

    Added some js files

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 // Listen for app to be ready
 app.on("ready", function() {
   // Create new window
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({ width: 1920, height: 1440 });
   // Load html into window
   mainWindow.loadURL(
     url.format({
@@ -18,4 +18,38 @@ app.on("ready", function() {
       slashes: true
     })
   );
+
+  //Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+  //Insert the mainmenu
+  Menu.setApplicationMenu(mainMenu);
 });
+
+const mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Quit",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  },
+  {
+    label: "Developer Tools",
+    submenu: [
+      {
+        label: "Toggle Dev Tools",
+        accelerator: process.platform == "darwin" ? "Command+I" : "Ctrl+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      { role: "reload" }
+    ]
+  }
+];
---
+++ window/jsfiles/directories.js
@@ -0,0 +1,33 @@
+const electron = require("electron");
+const { ipcRenderer } = electron;
+
+function dirPath() {
+  document.getElementById("demo").innerHTML = document.getElementById(
+    "inputfile"
+  ).files[0].path;
+  console.log(document.getElementById("inputfile").files[0].path);
+}
+
+function scanDirs() {
+  var fs = require("fs");
+  path = document.getElementById("demo").innerHTML;
+  console.log(path);
+  fs.readdir(path, function(err, items) {
+    for (var i = 0; i < items.length; i++) {
+      var file = path + "/" + items[i];
+
+      console.log("Start: " + file);
+      fs.stat(file, generate_callback(file));
+      fs.stat(file, function(err, stats) {
+        if (stats.isDirectory()) console.log("Directory");
+      });
+    }
+  });
+}
+
+function generate_callback(file) {
+  return function(err, stats) {
+    console.log(file);
+    console.log(stats["size"]);
+  };
+}
