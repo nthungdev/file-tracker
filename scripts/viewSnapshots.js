c61fca70369773a8a386dd436093ef1dd20f1a80
Hung Nguyen <nthungdev@gmail.com>
2019-04-11 00:55:51 -0400

    update viewSnapshots

--- scripts/viewSnapshots.js
+++ scripts/viewSnapshots.js
@@ -27,6 +27,7 @@ ipcRenderer.on("folder-path", (event, arg) => {
 // VARIABLES ///////////////////////////////////////////////////////
 let folderPath;
 let hasSavedSnapshot;
+let savedFileStats;
 
 console.log(1);
 // db.getAllSnapshots(rows => {
@@ -56,6 +57,54 @@ function loadSavedSnapshots() {
     console.log(4);
     console.log("folderPath: " + folderPath);
     // let fileStats = dir.getFileStatsInDir(folderPath);
+    db.getSnapshotInDir(folderPath, rows => {
+      console.log("rows: ");
+      console.log(rows);
+      savedFileStats = rows;
+      // console.log(rows.length === 0 ? "none" : rows);
+      if (rows.length === 0) {
+        hasSavedSnapshot = false;
+      } else {
+        rows.forEach(row => {
+          ids.forEach(id => {
+            element = document.querySelector(
+              `#${id} div.viewSnapshots--stats-snapshots-section-column`
+            );
+            if (element) {
+              // console.log(element);
+              if (id.includes("timeMs")) {
+                element.appendChild(createRowData(new Date(row[id])));
+              } else if (id === mod) {
+                // not sure if it need conversion or not. but the line below doesn't work
+                // element.appendChild(createRowData(row[id].toString(radix)));
+                element.appendChild(createRowData(row[id]));
+              } else {
+                element.appendChild(createRowData(row[id]));
+              }
+            }
+          });
+        });
+      }
+    });
+  }
+}
+
+function loadCurrentMedata() {
+  console.log("loadCurrentMedata");
+  let ids = ["fileName", "filePath", "size", "mtimeMs", "atimeMs", "mod"];
+
+  if (folderPath) {
+    console.log(4);
+    console.log("folderPath: " + folderPath);
+    let fileStats = dir.getFileStatsInDir(folderPath);
+
+    let trackedFiles = []
+    let unTrackedFiles = []
+
+    fileStats.forEach(file => {
+      if()
+    })
+
     db.getSnapshotInDir(folderPath, rows => {
       console.log("rows: ");
       console.log(rows);
